314eb6168908bcb0c8667eb9a5358059e4dbd263
Killian Gardahaut <killian.gardahaut@gmail.com>
2022-05-31 16:22:53 +0200

    Update move-your-div.js

--- move-your-div.js
+++ move-your-div.js
@@ -5,37 +5,42 @@ Quand la div revient à son point d'origine, un modal s'affiche.
 Le modal disparait lorsqu'on recommence le cycle.  
 Un deuxième bouton doit servir à masquer le modal.
 
+/!\ IN ENGLISH FOR FUCK SAKE
 */
-let move = document.getElementById("moveDiv");
-let hide = document.getElementById("hideModal")
+
+let move = document.getElementById("moveDiv");// Either you remove the ; here either you add it everywhere
+let hide = document.getElementById("hideModal")// like here
 let red = document.querySelector(".red");
 let modal = document.querySelector(".modal")
 
-let clickNbr = 0;
 hide.addEventListener("click", function(){
         modal.style.display = "none"
 })
+
+let clickNbr = 0;
 move.addEventListener("click", function(){
     clickNbr++;
     
-        
-    if (1 === clickNbr){
-      red.style.left = "100%"
-      modal.style.display ="none"
-    };
-    if (2 === clickNbr){
+    switch (clickNbr) {
+        case 1 :
+            red.style.left = "100%"
+            modal.style.display ="none"
+            break;
+        case 2 :
             red.style.right = "100%"
             red.style.top = "50%"
-    };
-    if (3 === clickNbr){
+            break;
+        case 3 :
             red.style.top = "50%"
             red.style.left = "0%"
-    }
-    if (4 === clickNbr){
+            break;
+        case 4 :
             red.style.left = "0%"
             red.style.top ="0%"
             modal.style.display = "block"
             clickNbr = 0
+            break;
+        default:
+            break;
     }
-   
 })
